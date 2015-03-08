cabe41275f4305b5213ccd127dba42e570eae3b5
jsuarezruiz <javiersuarezruiz@hotmail.com>
2015-03-08 18:10:53 +0100

    Updated Sample

--- CartujaDotNet/ALMdeando/Ejemplo/TipCalc/build.sh
+++ CartujaDotNet/ALMdeando/Ejemplo/TipCalc/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mono --runtime=v4.0 tools/NuGet/nuget.exe install FAKE -Version 3.5.4
-mono --runtime=v4.0 packages/FAKE.3.5.4/tools/FAKE.exe build.fsx $@
+nuget install FAKE -Version 3.5.4
+mono --runtime=v4.0 FAKE.3.5.4/tools/Fake.exe build.fsx $@
