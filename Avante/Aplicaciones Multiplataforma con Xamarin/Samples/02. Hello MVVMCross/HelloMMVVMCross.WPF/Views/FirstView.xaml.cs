06096851fc301357d32cbbc5d65b640226a6beaf
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-06-07 13:48:46 +0200

    Added Xamarin Samples

---
+++ Avante/Aplicaciones Multiplataforma con Xamarin/Samples/02. Hello MVVMCross/HelloMMVVMCross.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.Wpf.Views;
+
+namespace HelloMMVVMCross.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
