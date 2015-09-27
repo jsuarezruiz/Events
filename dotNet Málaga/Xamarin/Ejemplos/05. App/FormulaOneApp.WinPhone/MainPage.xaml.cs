60747cb7fea8b9ff45a00b0517779675b0cdbb03
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-09-27 17:42:10 +0200

    MVVMCross Formula One Sample

---
+++ dotNet Málaga/Xamarin/Ejemplos/05. App/FormulaOneApp.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace FormulaOneApp.WinPhone
+{
+    using Microsoft.Phone.Controls;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Platform.WinPhone;
+
+    public partial class MainPage : FormsApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SupportedOrientations = SupportedPageOrientation.PortraitOrLandscape;
+
+            Forms.Init();
+            OxyPlot.Xamarin.Forms.Platform.WP8.Forms.Init(); 
+            Xamarin.FormsMaps.Init();
+            LoadApplication(new FormulaOneApp.App());
+        }
+    }
+}
