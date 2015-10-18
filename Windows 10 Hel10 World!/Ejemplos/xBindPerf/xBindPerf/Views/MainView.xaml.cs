e3c98bd560f624f057455f6a9596f4ffb94bd2f3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-10-18 17:48:35 +0200

    Windows 10 Hel10 World Samples

---
+++ Windows 10 Hel10 World!/Ejemplos/xBindPerf/xBindPerf/Views/MainView.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace xBindPerf.Views
+{
+    using System;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnClear(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Content.Children.Clear();
+        }
+
+        private void OnBinding(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var date = DateTime.Now;
+            var binding = new Binding();
+            string diff;
+            binding.Loaded += (s, args) =>
+            {
+                diff = (DateTime.Now - date).ToString();
+                Diff.Text = diff;
+            };
+
+            Content.Children.Clear();
+            Content.Children.Add(binding);
+        }
+
+        private void OnXBinding(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var date = DateTime.Now;
+            var binding = new XBind();
+            string diff;
+            binding.Loaded += (s, args) =>
+            {
+                diff = (DateTime.Now - date).ToString();
+                Diff.Text = diff;
+            };
+
+            Content.Children.Clear();
+            Content.Children.Add(binding);
+        }
+    }
+}
