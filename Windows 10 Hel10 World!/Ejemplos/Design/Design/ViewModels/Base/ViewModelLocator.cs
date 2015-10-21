f93e4d81d1c989a6be3f1f4601b64b111d1eb9c3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-10-21 20:51:56 +0200

    Update Windows 10 Hel10 Word material

---
+++ Windows 10 Hel10 World!/Ejemplos/Design/Design/ViewModels/Base/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿namespace Design.ViewModels.Base
+{
+    using Microsoft.Practices.Unity;
+
+    public class ViewModelLocator
+    {
+        readonly IUnityContainer _container;
+
+        public ViewModelLocator()
+        {
+            _container = new UnityContainer();
+
+            // ViewModels
+            _container.RegisterType<ShellViewModel>();
+            _container.RegisterType<RestaurantViewModel>();
+		}
+
+        public ShellViewModel ShellViewModel => _container.Resolve<ShellViewModel>();
+        public RestaurantViewModel RestaurantViewModel => _container.Resolve<RestaurantViewModel>();
+	}
+}
