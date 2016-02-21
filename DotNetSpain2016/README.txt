d301832ae3e7e0597897301609ff15739508b4e3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-02-21 18:50:23 +0100

    dotNetSpain2016 Samples

---
+++ DotNetSpain2016/README.txt
@@ -0,0 +1,8 @@
+FAKE
+https://marketplace.visualstudio.com/items/isaacabraham.fsharp-helpers-extension
+
+HockeyApp
+https://marketplace.visualstudio.com/items?itemName=ms.hockeyapp
+
+Test Cloud
+https://marketplace.visualstudio.com/items?itemName=Xamarin.vso-xtc-results
---
+++ DotNetSpain2016/Samples/Calculator/Calculator/Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Calculator.Droid
+{
+	[Activity (Label = "Calculator.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Xamarin.Forms.Forms.ViewInitialized += (sender,  e) => 
+			{
+				if (!string.IsNullOrWhiteSpace(e.View.StyleId)) 
+				{
+					e.NativeView.ContentDescription = e.View.StyleId;
+				}
+			};
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+		}
+	}
+}
+
