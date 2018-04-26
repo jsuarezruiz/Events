8fc71069acef6317ae897b7ade0b0db2541cc6e4
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-04-26 21:18:53 +0200

    Added Xamarin.Forms 3.0 Preview Samples

---
+++ Primer vistazo a Xamarin.Forms 3.0/Samples/Movies/Movies/Movies/Converters/MenuItemTypeConverter.cs
@@ -0,0 +1,80 @@
+﻿using Movies.Models;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Movies.Converters
+{
+    public class MenuItemTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var menuItemType = (MenuItemType)value;
+
+            switch (menuItemType)
+            {
+                case MenuItemType.Discover:
+                    if (Device.RuntimePlatform == Device.GTK || Device.RuntimePlatform == Device.WPF)
+                        return "Images/movies-discover.png";
+                    else if (Device.RuntimePlatform == Device.UWP)
+                        return "Assets/movies-discover.png";
+                    else if (Device.RuntimePlatform == Device.Android || Device.RuntimePlatform == Device.iOS)
+                        return "movies_discover";
+                    else
+                        return string.Empty;
+                case MenuItemType.Movies:
+                    if (Device.RuntimePlatform == Device.GTK || Device.RuntimePlatform == Device.WPF)
+                        return "Images/movies-movie.png";
+                    else if (Device.RuntimePlatform == Device.UWP)
+                        return "Assets/movies-movie.png";
+                    else if (Device.RuntimePlatform == Device.Android || Device.RuntimePlatform == Device.iOS)
+                        return "movies_movie";
+                    else
+                        return string.Empty;
+                case MenuItemType.TVShows:
+                    if (Device.RuntimePlatform == Device.GTK || Device.RuntimePlatform == Device.WPF)
+                        return "Images/movies-shows.png";
+                    else if (Device.RuntimePlatform == Device.UWP)
+                        return "Assets/movies-shows.png";
+                    else if (Device.RuntimePlatform == Device.Android || Device.RuntimePlatform == Device.iOS)
+                        return "movies_shows";
+                    else
+                        return string.Empty;
+                case MenuItemType.Upcoming:
+                    if (Device.RuntimePlatform == Device.GTK || Device.RuntimePlatform == Device.WPF)
+                        return "Images/movies-upcoming.png";
+                    else if (Device.RuntimePlatform == Device.UWP)
+                        return "Assets/movies-upcoming.png";
+                    else if (Device.RuntimePlatform == Device.Android || Device.RuntimePlatform == Device.iOS)
+                        return "movies_upcoming";
+                    else
+                        return string.Empty;
+                case MenuItemType.Favourites:
+                    if (Device.RuntimePlatform == Device.GTK || Device.RuntimePlatform == Device.WPF)
+                        return "Images/movies-favourites.png";
+                    else if (Device.RuntimePlatform == Device.UWP)
+                        return "Assets/movies-favourites.png";
+                    else if (Device.RuntimePlatform == Device.Android || Device.RuntimePlatform == Device.iOS)
+                        return "movies_favourites";
+                    else
+                        return string.Empty;
+                case MenuItemType.Settings:
+                    if (Device.RuntimePlatform == Device.GTK || Device.RuntimePlatform == Device.WPF)
+                        return "Images/movies-settings.png";
+                    else if (Device.RuntimePlatform == Device.UWP)
+                        return "Assets/movies-settings.png";
+                    else if (Device.RuntimePlatform == Device.Android || Device.RuntimePlatform == Device.iOS)
+                        return "movies_settings";
+                    else
+                        return string.Empty;
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Primer vistazo a Xamarin.Forms 3.0/Samples/Movies/Movies/Movies/ViewModels/Base/Locator.cs
@@ -0,0 +1,65 @@
+﻿using Autofac;
+using Movies.Services.Movies;
+using Movies.Services.Navigation;
+using Movies.Services.People;
+using Movies.Services.Request;
+using Movies.Services.TVShow;
+using System;
+
+namespace Movies.ViewModels.Base
+{
+    public class Locator
+    {
+        private static IContainer _container;
+
+        private static readonly Locator _instance = new Locator();
+
+        public static Locator Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        protected Locator()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<NavigationService>().As<INavigationService>().SingleInstance();
+            builder.RegisterType<RequestService>().As<IRequestService>();
+            builder.RegisterType<MoviesService>().As<IMoviesService>();
+            builder.RegisterType<PeopleService>().As<IPeopleService>();
+            builder.RegisterType<TVShowService>().As<ITVShowService>();
+
+            builder.RegisterType<DetailViewModel>();
+            builder.RegisterType<GalleryViewModel>();
+            builder.RegisterType<MoviesViewModel>();
+            builder.RegisterType<MenuViewModel>();
+            builder.RegisterType<MainViewModel>();
+            builder.RegisterType<HomeViewModel>();
+            builder.RegisterType<HomepageViewModel>();
+            builder.RegisterType<PeopleViewModel>();
+            builder.RegisterType<ShowsViewModel>();
+            builder.RegisterType<SplashViewModel>();
+            builder.RegisterType<UpcomingViewModel>();
+
+            if (_container != null)
+            {
+                _container.Dispose();
+            }
+
+            _container = builder.Build();
+        }
+
+        public T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        public object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+    }
+}
