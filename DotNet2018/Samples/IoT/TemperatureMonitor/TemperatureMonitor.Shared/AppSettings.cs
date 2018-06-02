5be12c0f68cd8f43809621bf98e781c4fc30a7cf
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-06-02 18:11:31 +0200

    Updated Azure Data

--- DotNet2018/Samples/IoT/TemperatureMonitor/TemperatureMonitor.Shared/AppSettings.cs
+++ DotNet2018/Samples/IoT/TemperatureMonitor/TemperatureMonitor.Shared/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class AppSettings
     {
-        public static string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=temperaturemonitor;AccountKey=1E2nvlChXwX5LygucCgVGHwDo/iJJ2QjzBd5GlcELX09H64lccZo6mxOE6onUjJGxcz2IZpfqZN38204ly+w7Q==;EndpointSuffix=core.windows.net";
+        public static string StorageConnectionString = "INSERT YOUR STORAGE CONNECTION HERE";
         public static string StorageTableName = "TemperatureMonitor";
         public static string StoragePartitionKey = "TemperatureKey";
         public static string ApiUrl = "http://temperaturemonitorapi.azurewebsites.net";
